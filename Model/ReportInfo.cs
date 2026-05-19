c53050e4b6210ffc6645be678a38063ac0be2d31
Stephen I. Dumili <102124249+stephendumili0911@users.noreply.github.com>
2026-05-19 11:46:53 +0800

    MIS changes - 05-19-2026
    1. Modify on ReportInfo.cs, added RecordCount, TransCount

--- Model/ReportInfo.cs
+++ Model/ReportInfo.cs
@@ -12,7 +12,8 @@ namespace MIS.Model
         public ListView ListView { get; set; }
         public string SheetName { get; set; }      // Tab name
         public string Title { get; set; }          // Header title
-        public string Count { get; set; }          // Count
+        public string RecordCount { get; set; }          // Record Count
+        public string TransCount { get; set; }          // Trans Count
         public string Total { get; set; }          // Total amount
     }
 
